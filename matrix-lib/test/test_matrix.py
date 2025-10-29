385bb71e01c0dc71483d612a7edc73c7b3d8d4fb
texample-42 <texample42@gmail.com>
2025-10-29 22:22:49 +0200

    Create test_matrix.py

---
+++ matrix-lib/test/test_matrix.py
@@ -0,0 +1,26 @@
+from Matrix import Matrix
+
+def test_addition():
+    A = Matrix([[1, 2], [3, 4]])
+    B = Matrix([[1, 1], [1, 1]])
+    C = A.add(B)
+    assert C.data == [[2, 3], [4, 5]]
+def test_sutraction():
+    A = Matrix([[1, 2], [3, 4]])
+    B = Matrix([[1, 1], [1, 1]])
+    C = A.subtract(B)
+    assert C.data == [[0, 1], [2, 3]]
+def test_multiply():
+    A = Matrix([[1, 2], [3, 4]])
+    B = Matrix([[1, 1], [1, 1]])
+    C = A.multiply(B)
+    assert C.data == [[3, 3], [7, 7]]
+def test_scalar_multiply():
+    A = Matrix([[1, 2], [3, 4]])
+    B = 3
+    C = A.scalar_multiply(B)
+    assert C.data ==[[3, 6],[9, 12]]
+def test_trace():
+    A = Matrix([[1, 2], [3, 4]])
+    B=A.trace()
+    assert B==5
